3a25f49420ea877271a8d6f3662134b1ac89172e
EvinceMoi <baneyue@gmail.com>
2021-08-08 16:41:04 +0800

    use relative pos for each part

--- src/main.cpp
+++ src/main.cpp
@@ -125,16 +125,17 @@ void ShowConsole(bool show) {
 }
 
 struct display {
-	ID3D11Device* d3d_device_;
-	ID3D11DeviceContext* d3d_device_context_;
-	IDXGISwapChain* swap_chain_;
-	ID3D11RenderTargetView* main_render_target_view_;
 	WNDCLASSEX wc_;
 	HWND hwnd_;
 	bool abort_;
 	telemetry_data_t data_;
+	ImFont* font_normal_;
+	ImFont* font_big_;
+	ImFont* font_small_;
+	ImColor color_black{ 0x00, 0x00, 0x00, 0xFF };
+	ImColor color_white{ 0xFF, 0xF0, 0xF0, 0xFF };
 
-	display(): abort_(false) {}
+	display() : abort_(false), data_{ 0.f } {}
 	bool init() {
 		abort_ = false;
 		wc_ = { 
@@ -182,14 +183,31 @@ struct display {
 		ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
 
 		//ImGui_ImplWin32_EnableDpiAwareness();
+		
+		auto io = ImGui::GetIO();
 
+		io.WantCaptureMouse = true;
+		io.WantCaptureKeyboard = true;
 
+		font_normal_ = io.Fonts->AddFontFromMemoryCompressedTTF(
+			CascadiaMonoPL_compressed_data,
+			CascadiaMonoPL_compressed_size,
+			28.f
+		);
+		font_big_ = io.Fonts->AddFontFromMemoryCompressedTTF(
+			CascadiaMonoPL_compressed_data,
+			CascadiaMonoPL_compressed_size,
+			80.f
+		);
+		font_small_ = io.Fonts->AddFontFromMemoryCompressedTTF(
+			CascadiaMonoPL_compressed_data,
+			CascadiaMonoPL_compressed_size,
+			16.f
+		);
 
 		return true;
 	}
 	void show() {
-		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
 		data_.speed = 43.4f;
 		data_.gear = 5;
 		data_.stear = -0.4;
@@ -201,34 +219,7 @@ struct display {
 		data_.track_length = 16823.f;
 		data_.lap_distance = 2438.f;
 
-		data_.speed = 0.f;
-		data_.gear = 0.f;
-		data_.stear = 0.f;
-		data_.clutch = 0.f;
-		data_.brake = 0.f;
-		data_.throttle = 0.f;
-		data_.rpm = 0.f;
-		data_.max_rpm = 0.f;
-		data_.track_length = 0.f;
-		data_.lap_distance = 0.f;
-
 		auto io = ImGui::GetIO();
-
-		auto font1 = io.Fonts->AddFontFromMemoryCompressedTTF(
-			CascadiaMonoPL_compressed_data, 
-			CascadiaMonoPL_compressed_size, 
-			28.f
-		);
-		auto font2 = io.Fonts->AddFontFromMemoryCompressedTTF(
-			CascadiaMonoPL_compressed_data, 
-			CascadiaMonoPL_compressed_size, 
-			80.f
-		);
-		auto font_fps = io.Fonts->AddFontFromMemoryCompressedTTF(
-			CascadiaMonoPL_compressed_data, 
-			CascadiaMonoPL_compressed_size, 
-			16.f
-		);
 
 		while (!abort_) {
 			MSG msg;
@@ -240,6 +231,8 @@ struct display {
 			}
 			if (abort_) break;
 
+			handle_input();
+
 			ImGui_ImplDX11_NewFrame();
 			ImGui_ImplWin32_NewFrame();
 			ImGui::NewFrame();
@@ -249,223 +242,31 @@ struct display {
 				auto io = ImGui::GetIO();
 				ImGui::SetNextWindowPos(ImVec2(0, 0));
 				ImGui::SetNextWindowSize(io.DisplaySize);
-				// ImGui::Begin("Display", 0, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoDecoration);
 
 				ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
 				ImGui::Begin("Display", 0, ImGuiWindowFlags_NoDecoration);
 
-				io.WantCaptureMouse = true;
-				io.WantCaptureKeyboard = true;
-
+				auto win_x = io.DisplaySize.x; 
+				auto win_y = io.DisplaySize.y;
+				
 				auto drawList = ImGui::GetWindowDrawList();
-				auto win_x = io.DisplaySize.x; auto win_y = io.DisplaySize.y;
-				auto color_black = ImColor(0x00, 0x00, 0x00, 0xFF);
-				auto color_white = ImColor(0xFF, 0xF0, 0xF0, 0xFF);
-				// BG
-				{
-					drawList->AddRectFilled(ImVec2(), ImVec2(win_x, win_y), color_black);
-				}
+				drawList->AddRectFilled(ImVec2(), ImVec2(win_x, win_y), color_black); // background
+
 				// RPM
-				{
-					auto sy = 0;
-					auto ey = win_y * 0.2;
-					auto max_rpm_sep = static_cast<uint8_t>(std::floor(data_.max_rpm / 1000)) + 1;
-					auto percent = data_.rpm / (max_rpm_sep * 1000);
-
-					auto color_green = ImColor(0x00, 0xFF, 0x00, 0xFF);
-					auto label_len = .12f;
-
-					// draw bar
-					{
-						// mask
-						auto sx = win_x * label_len;
-						auto ex = win_x;
-						std::array<float, 4> pos_stop = { 0.f, 0.4f, 0.64f, 1.f };
-						std::array<ImColor, 4> color_stop = {
-							color_green,
-							ImColor(0xE0, 0xFF, 0x00, 0xFF),
-							ImColor(0xFF, 0xF8, 0x00, 0xFF),
-							ImColor(0xFF, 0x00, 0x00, 0xFF),
-						};
-						for (int i = 0; i < 3; i++) {
-							auto cl = color_stop[i];
-							auto cr = color_stop[i + 1];
-							auto p_tl = ImVec2(sx + (ex - sx) * pos_stop[i], sy);
-							auto p_br = ImVec2(sx + (ex - sx) * pos_stop[i + 1], ey);
-							drawList->AddRectFilledMultiColor(p_tl, p_br, cl, cr, cr, cl);
-						}
-						// real mask
-						{
-							auto ps = ImVec2(sx + (ex - sx) * percent, sy);
-							auto pe = ImVec2(ex, ey);
-							drawList->AddRectFilled(ps, pe, color_black);
-						}
-					}
-					// daw rpm label
-					{
-						drawList->AddRectFilled(
-							ImVec2(),
-							ImVec2(win_x * label_len, ey),
-							color_green
-						);
-						// draw RPM text
-						static const char* label = "RPM";
-						auto tsize = ImGui::CalcTextSize(label);
-						//auto pos = ImVec2((win_x * label_len - tsize.x) / 2, (ey - tsize.y) / 2); // center
-						auto pos = ImVec2(tsize.x / 3, (ey - tsize.y) / 2); // left
-						drawList->AddText(pos, color_black, label);
-
-					}
-
-					// draw sep
-					{
-						auto tsize = ImGui::CalcTextSize("0");
-						for (auto i = 0; i < max_rpm_sep; i++) {
-							if ((i + 1) == max_rpm_sep) break;
-							auto p = (i + 1) * 1.f / max_rpm_sep;
-							auto x = win_x * label_len + win_x * (1 - label_len) * p - 1 * tsize.x;
-							auto pos = ImVec2(x, (ey - tsize.y) / 2);
-							auto t = std::to_string(i + 1);
-							drawList->AddText(pos, color_black, t.c_str());
-						}
-					}
-				}
+				draw_section_rpm(ImVec2(), ImVec2(win_x, win_y * 0.2));
 				// Speed / Gear / Hand Brake
-				{
-					auto sy = win_y * 0.2;
-					auto ey = win_y * 0.8;
-					auto pos_tl = ImVec2(0, sy);
-					auto pos_br = ImVec2(win_x, ey);
-
-					// background
-					drawList->AddRectFilled(pos_tl, pos_br, color_white);
-
-					auto p_speed = 0.3;
-					auto p_kmh = 0.3;
-					auto p_gear = 0.2;
-					auto p_hb = 0.2;
-					// draw speed
-					{
-						auto speed_kph = std::to_string(static_cast<uint32_t>(std::floor(data_.speed * 3.6)));
-						if (speed_kph.size() < 3) {
-							auto prefix = 3 - speed_kph.size();
-							speed_kph = std::string(2, ' ') + speed_kph;
-						}
-						ImGui::PushFont(font2);
-						auto tsize = ImGui::CalcTextSize(speed_kph.c_str());
-						auto pos = ImVec2((win_x * p_speed - tsize.x), sy + (ey - sy - tsize.y) / 2);
-						drawList->AddText(pos, color_black, speed_kph.c_str());
-						ImGui::PopFont();
-					}
-
-					// draw km/h
-					{
-						static const char* kmh = "KM/H";
-						auto tsize = ImGui::CalcTextSize(kmh);
-						auto pos = ImVec2(win_x * p_speed + win_x * p_kmh * 0.2, sy + (ey - sy) / 2);
-						drawList->AddText(pos, color_black, kmh);
-					}
-
-					// draw gear
-					{
-						auto tl = ImVec2(win_x * (p_speed + p_kmh), sy);
-						auto br = ImVec2(win_x * (p_speed + p_kmh + p_gear), ey);
-						auto color_gray = ImColor(0x69, 0x69, 0x69, 0xFF);
-						drawList->AddRectFilled(tl, br, color_gray);
-
-						ImGui::PushFont(font2);
-						auto gear = std::to_string(static_cast<uint8_t>(std::floor(data_.gear)));
-						auto tsize = ImGui::CalcTextSize("0");
-						auto pos = ImVec2(tl.x + (win_x * p_gear - tsize.x) / 2, sy + (ey - sy - tsize.y) / 2);
-						drawList->AddText(pos, color_black, gear.c_str());
-						ImGui::PopFont();
-					}
-					
-				}
+				draw_section_speed(ImVec2(0, win_y * 0.2), ImVec2(win_x, win_y * 0.8));
 				// Input(Brake/Throttle)
-				{
-					auto sy = win_y * 0.8;
-					auto ey = win_y;
-					auto pos_tl = ImVec2(0, sy);
-					auto pos_br = ImVec2(win_x, ey);
-
-					auto color_brake = ImColor(0xBD, 0x00, 0x00, 0xFF);
-					auto color_throttle = ImColor(0x25, 0xBD, 0x00, 0xFF);
-
-					auto brake_tl = ImVec2((1 - data_.brake) * win_x / 2, sy);
-					auto brake_br = ImVec2(win_x / 2, ey);
-					drawList->AddRectFilled(brake_tl, brake_br, color_brake);
-
-					auto throttle_tl = ImVec2(win_x / 2, sy);
-					auto throttle_br = ImVec2((1 + data_.throttle) * win_x / 2, ey);
-					drawList->AddRectFilled(throttle_tl, throttle_br, color_throttle);
-
-					// sep
-					static ImColor c_sep{ 1.f, 1.f, 1.f, 0.1f };
-					drawList->AddLine(
-						ImVec2(win_x / 2, sy),
-						ImVec2(win_x / 2, ey),
-						c_sep
-					);
-
-
-					static const char* brakes = " BRAKES ";
-					static const char* throttle = " THROTTLE ";
-					auto tsize_b = ImGui::CalcTextSize(brakes);
-					auto tsize_t = ImGui::CalcTextSize(throttle);
-					auto pos_b = ImVec2(win_x / 2 - tsize_b.x, sy + (ey - sy - tsize_b.y) / 2);
-					auto pos_t = ImVec2(win_x / 2, sy + (ey - sy - tsize_t.y) / 2);
-					drawList->AddText(pos_b, color_white, brakes);
-					drawList->AddText(pos_t, color_white, throttle);
-				}
-				{
-					if (io.MouseClicked[1]) {
-						// right click
-					}
-
-					static bool drag_moving = false;
-					static ImVec2 pos;
-					float cdelta = .00001f;
-					if (io.MouseDown[0] && io.MouseDownDuration[0] < cdelta) {
-						// mouse left down
-						drag_moving = true;
-						pos = io.MousePos;
-					}
-					if (io.MouseReleased[0]) {
-						drag_moving = false;
-					}
-					if (drag_moving) {
-						auto p = io.MousePos;
-						auto dx = p.x - pos.x;
-						auto dy = p.y - pos.y;
-						move_window(dx, dy);
-					}
-					{
-						// frame rate
-						ImGui::PushFont(font_fps);
-						auto s = std::to_string(static_cast<int>(io.Framerate));
-						if (s.size() < 3) {
-							auto prefix = 3 - s.size();
-							s = std::string(2, ' ') + s;
-						}
-						auto tsize = ImGui::CalcTextSize(s.c_str());
-						static ImColor c_fps{1.f, 1.f, 1.f, 0.4f};
-						drawList->AddText(ImVec2(win_x - tsize.x / 3 * 4, tsize.x / 3), c_fps, s.c_str());
-						ImGui::PopFont();
-					}
-
-					{
-						if (io.KeysDown[VK_ESCAPE]) {
-							abort_ = true;
-						}
-					}
-				}
+				draw_section_input(ImVec2(0, win_y * 0.8), ImVec2(win_x, win_y));
+				// fps
+				draw_fps(ImVec2(), ImVec2(win_x, win_y));
 
 				ImGui::End();
 				ImGui::PopStyleVar();
 			}
 
 			ImGui::Render();
+			static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 			const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
 			g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
 			g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
@@ -483,6 +284,214 @@ struct display {
 			::UnregisterClass(wc_.lpszClassName, wc_.hInstance);
 		}
 	}
+	void handle_input() {
+		auto io = ImGui::GetIO();
+
+		if (io.MouseClicked[1]) {
+			// right click
+		}
+
+		{
+			// drag move
+			static bool drag_moving = false;
+			static ImVec2 pos;
+			float cdelta = .00001f;
+			if (io.MouseDown[0] && io.MouseDownDuration[0] < cdelta) {
+				// mouse left down
+				drag_moving = true;
+				pos = io.MousePos;
+			}
+			if (io.MouseReleased[0]) {
+				drag_moving = false;
+			}
+			if (drag_moving) {
+				auto p = io.MousePos;
+				auto dx = p.x - pos.x;
+				auto dy = p.y - pos.y;
+				move_window(dx, dy);
+			}
+
+		}
+		{
+			// ESC to quit
+			if (io.KeysDown[VK_ESCAPE]) {
+				abort_ = true;
+			}
+		}
+	}
+	void draw_section_rpm(ImVec2 tl, ImVec2 br) {
+
+		auto drawList = ImGui::GetWindowDrawList();
+
+		auto w = br.x - tl.x;
+		auto h = br.y - tl.y;
+
+		auto max_rpm_step = static_cast<uint8_t>(std::floor(data_.max_rpm / 1000)) + 1;
+		auto p = data_.rpm / (max_rpm_step * 1000);
+
+		static auto color_green = ImColor(0x00, 0xFF, 0x00, 0xFF);
+		auto label_len = .12f;
+
+
+		// draw rpm label
+		{
+			drawList->AddRectFilled(
+				tl,
+				ImVec2(tl.x + w * label_len, tl.y + h),
+				color_green
+			);
+			// draw RPM text
+			static const char* label = "RPM";
+			auto tsize = ImGui::CalcTextSize(label);
+			auto pos = ImVec2(tl.x + (w * label_len - tsize.x) / 2, tl.y + (h - tsize.y) / 2); // center
+			//auto pos = ImVec2(tl.x + tsize.x / 3, tl.y + (h - tsize.y) / 2); // left
+			drawList->AddText(pos, color_black, label);
+
+		}
+		// draw bar
+		{
+			// mask
+			auto sx = tl.x + w * label_len;
+			auto ex = br.x;
+			std::array<float, 4> pos_stop = { 0.f, 0.4f, 0.64f, 1.f };
+			std::array<ImColor, 4> color_stop = {
+				color_green,
+				ImColor(0xE0, 0xFF, 0x00, 0xFF),
+				ImColor(0xFF, 0xF8, 0x00, 0xFF),
+				ImColor(0xFF, 0x00, 0x00, 0xFF),
+			};
+			for (int i = 0; i < 3; i++) {
+				auto cl = color_stop[i];
+				auto cr = color_stop[i + 1];
+				auto p_tl = ImVec2(sx + (ex - sx) * pos_stop[i], tl.y);
+				auto p_br = ImVec2(sx + (ex - sx) * pos_stop[i + 1], br.y);
+				drawList->AddRectFilledMultiColor(p_tl, p_br, cl, cr, cr, cl);
+			}
+			// real mask
+			{
+				auto ps = ImVec2(sx + (ex - sx) * p, tl.y);
+				auto pe = ImVec2(ex, br.y);
+				drawList->AddRectFilled(ps, pe, color_black);
+			}
+		}
+
+		// draw step
+		{
+			auto tsize = ImGui::CalcTextSize("0");
+			for (auto i = 0; i < max_rpm_step; i++) {
+				if ((i + 1) == max_rpm_step) break;
+				auto p = (i + 1) * 1.f / max_rpm_step;
+				auto x = tl.x + w * label_len + w * (1 - label_len) * p - 1 * tsize.x;
+				auto pos = ImVec2(x, tl.y + (h - tsize.y) / 2);
+				auto t = std::to_string(i + 1);
+				drawList->AddText(pos, color_black, t.c_str());
+			}
+		}
+	}
+	void draw_section_speed(ImVec2 tl, ImVec2 br) {
+		auto drawList = ImGui::GetWindowDrawList();
+
+		auto w = br.x - tl.x;
+		auto h = br.y - tl.y;
+
+		// background
+		drawList->AddRectFilled(tl, br, color_white);
+
+		auto p_speed = 0.3;
+		auto p_kmh = 0.3;
+		auto p_gear = 0.2;
+		auto p_hb = 0.2;
+		// draw speed
+		{
+			auto speed_kph = std::to_string(static_cast<uint32_t>(std::floor(data_.speed * 3.6)));
+			if (speed_kph.size() < 3) {
+				auto prefix = 3 - speed_kph.size();
+				speed_kph = std::string(2, ' ') + speed_kph;
+			}
+			ImGui::PushFont(font_big_);
+			auto tsize = ImGui::CalcTextSize(speed_kph.c_str());
+			auto pos = ImVec2(tl.x + w * p_speed - tsize.x, tl.y + (h - tsize.y) / 2);
+			drawList->AddText(pos, color_black, speed_kph.c_str());
+			ImGui::PopFont();
+		}
+
+		// draw km/h
+		{
+			static const char* kmh = "KM/H";
+			auto tsize = ImGui::CalcTextSize(kmh);
+			auto pos = ImVec2(tl.x + w * p_speed + w * p_kmh * 0.2, tl.y + h / 2);
+			drawList->AddText(pos, color_black, kmh);
+		}
+
+		// draw gear
+		{
+			auto ptl = ImVec2(tl.x + w * (p_speed + p_kmh), tl.y);
+			auto pbr = ImVec2(tl.x + w * (p_speed + p_kmh + p_gear), tl.y + h);
+			auto color_gray = ImColor(0x69, 0x69, 0x69, 0xFF);
+			drawList->AddRectFilled(ptl, pbr, color_gray);
+
+			ImGui::PushFont(font_big_);
+			auto gear = std::to_string(static_cast<uint8_t>(std::floor(data_.gear)));
+			auto tsize = ImGui::CalcTextSize("0");
+			auto pos = ImVec2(ptl.x + (w * p_gear - tsize.x) / 2, tl.y + (h - tsize.y) / 2);
+			drawList->AddText(pos, color_black, gear.c_str());
+			ImGui::PopFont();
+		}
+	}
+	void draw_section_input(ImVec2 tl, ImVec2 br) {
+		auto drawList = ImGui::GetWindowDrawList();
+
+		auto w = br.x - tl.x;
+		auto h = br.y - tl.y;
+
+		static auto color_brake = ImColor(0xBD, 0x00, 0x00, 0xFF);
+		static auto color_throttle = ImColor(0x25, 0xBD, 0x00, 0xFF);
+
+		auto brake_tl = ImVec2(tl.x + (1 - data_.brake) * w / 2, tl.y);
+		auto brake_br = ImVec2(tl.x + w / 2, br.y);
+		drawList->AddRectFilled(brake_tl, brake_br, color_brake);
+
+		auto throttle_tl = ImVec2(tl.x + w / 2, tl.y);
+		auto throttle_br = ImVec2(tl.x + (1 + data_.throttle) * w / 2, tl.y + h);
+		drawList->AddRectFilled(throttle_tl, throttle_br, color_throttle);
+
+		// separator
+		static ImColor c_sep{ 1.f, 1.f, 1.f, 0.1f };
+		drawList->AddLine(
+			ImVec2(tl.x + w / 2, tl.y),
+			ImVec2(tl.x + w / 2, tl.y + h),
+			c_sep
+		);
+
+
+		static const char* brakes = " BRAKES ";
+		static const char* throttle = " THROTTLE ";
+		auto tsize_b = ImGui::CalcTextSize(brakes);
+		auto tsize_t = ImGui::CalcTextSize(throttle);
+		auto pb = ImVec2(tl.x + w / 2 - tsize_b.x, tl.y + (h - tsize_b.y) / 2);
+		auto pt = ImVec2(tl.x + w / 2, tl.y + (h - tsize_t.y) / 2);
+		drawList->AddText(pb, color_white, brakes);
+		drawList->AddText(pt, color_white, throttle);
+	}
+	void draw_fps(ImVec2 tl, ImVec2 br) {
+		// frame rate
+		auto io = ImGui::GetIO();
+		auto drawList = ImGui::GetWindowDrawList();
+
+		auto w = br.x - tl.x;
+
+		ImGui::PushFont(font_small_);
+		auto s = std::to_string(static_cast<int>(io.Framerate));
+		if (s.size() < 3) {
+			auto prefix = 3 - s.size();
+			s = std::string(2, ' ') + s;
+		}
+		auto tsize = ImGui::CalcTextSize(s.c_str());
+		static ImColor c_fps{ 1.f, 1.f, 1.f, 0.1f };
+		drawList->AddText(ImVec2(tl.x + w - tsize.x / 3 * 4, tsize.x / 3), c_fps, s.c_str());
+		ImGui::PopFont();
+	}
+	
 	void move_window(float dx, float dy) {
 		RECT rect;
 		GetWindowRect(hwnd_, &rect);
